b12605057d153c676aabed29e35a726c3cec71e0
Hack Reactor Student <student@hackreactor.com>
2017-06-10 17:15:20 -0700

    Adds working GET request.

--- web/request-handler.js
+++ web/request-handler.js
@@ -8,36 +8,6 @@ exports.handleRequest = function (req, res) {
   //default file of index.html
   var file;
   var statusCode;
-  //slice url from first index
-  // console.log(req.url);
-  // if(req.url === '/') {
-  //   file = archive.paths.siteAssets + '/index.html';
-  //   fs.readFile(file, function (err, data) {
-  //     res.writeHead(200, {'Content-Type': 'text/html'});
-  //     res.write(data);
-  //     res.end();
-  //   });
-  // }
-  // req.url = req.url.slice(1);
-  // //check status after calling isurlinlist 
-  // if (req.url !== '/') {
-  //   archive.isUrlInList(req.url, function(status) {
-  //     if (status) {
-  //       //go to request url
-  //       file = archive.paths.archivedSites + '/' + req.url;
-  //     } else {
-  //       //else loading.html
-  //       file = archive.paths.siteAssets + '/loading.html';
-  //     }
-  //     //readfile data display it
-  //     fs.readFile(file, function (err, data) {
-  //       res.writeHead(200, {'Content-Type': 'text/html'});
-  //       res.write(data);
-  //       res.end();
-  //     });
-  //   });
-  // }
-
   if (req.method === 'GET') {
     req.url = req.url.slice(1);
     //check status after calling isurlinlist 
@@ -61,7 +31,6 @@ exports.handleRequest = function (req, res) {
       });
     }
   } else {
-    console.log("hello");
   }
   
   
